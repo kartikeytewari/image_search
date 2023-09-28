a3d6412214343f52dc24e49a9c1f0c086c0a27b7
Kartikey Tewari <kartikeya30@gmail.com>
2023-09-28 23:43:50 +0530

    add search functionality

---
+++ app/test.py
@@ -0,0 +1,31 @@
+import weaviate
+import pybase64
+import json
+
+client = weaviate.Client(
+    url="http://localhost:8080"
+)
+
+# read the test image
+test_image = "dog-2.png"
+with open("../data/test/" + test_image, "rb") as local_image:
+    image_embedding = pybase64.b64encode(local_image.read())
+    image_embedding = image_embedding.decode('utf-8')
+
+    result_embedding = (
+        client.query
+        .get("meme_image", ["meme_image", "meme_text"])
+        .with_near_image({
+            "image": image_embedding
+        }, encode=False)
+        .with_limit(1)
+        .do()
+    )
+
+    result_image = result_embedding["data"]["Get"]["Meme_image"][0]["meme_image"]
+    result_text = result_embedding["data"]["Get"]["Meme_image"][0]["meme_text"]
+    
+    result_image = pybase64.b64decode((result_image))
+    img_file = open('out.jpeg', 'wb')
+    img_file.write(result_image)
+    img_file.close()
